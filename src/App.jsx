311bbfbaf3550b9111a0c17207df7837d2e659eb
Egor <egordelovayakolbasa@yandex.ru>
2024-09-18 23:32:34 +0300

    1 version

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 import cl from "./App.module.css";
+
 function App() {
 
   return (
